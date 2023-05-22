a464544cb9976a59d0d352f74140eebf5879e4a6
Zarana Patel <patelzarana@gmail.com>
2023-05-22 11:24:37 -0700

    Update AbstractPattern.js

--- AbstractPattern.js
+++ AbstractPattern.js
@@ -1,3 +1,16 @@
+/*
+What is Abstract Pattern ?
+An Abstract Factory creates objects that are related by a common theme. 
+In object-oriented programming, a Factory is an object that creates other objects. 
+An Abstract Factory has abstracted out a theme which is shared by the newly created objects.
+
+JavaScript does not support class-based inheritance therefore the abstract classes as depicted in the diagram are not used in the JavaScript example.
+Abstract classes and interfaces enforce consistent interfaces in derived classes.
+
+*/
+
+
+
 class Car{
      constructor(doors,engine,color){
           this.doors = doors;
